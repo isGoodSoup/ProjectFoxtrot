aa30b3cf3f66035fb90fef484c33978024ec3e20
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-01 13:24:46 +0100

    feat: from LineReader to Scanner

--- src/main/java/com/projectfoxtrot/main/FileCreator.java
+++ src/main/java/com/projectfoxtrot/main/FileCreator.java
@@ -3,51 +3,31 @@ package com.projectfoxtrot.main;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
+import java.util.Scanner;
 
 import com.projectfoxtrot.utils.Utils;
 
 public class FileCreator {
-	private Terminal terminal;
-	private LineReader reader;
 	private String home, file, path, extension;
 	private String new_line;
+	private Scanner scan = new Scanner(System.in);
 	private Utils util = new Utils();
-	
-	public FileCreator() {
-		try {
-			terminal = TerminalBuilder.builder().system(true).build();
-			reader = LineReaderBuilder.builder()
-		            .terminal(terminal)
-		            .build();
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
 	
 	public void newFile() throws IOException {
 		System.out.println("NEW FILE");
 		home = System.getenv("USERPROFILE");
-		file = util.toScan(reader, "Name");
-		extension = "." + util.toScan(reader, "Extension");
+		file = util.toScan(scan, "Name");
+		extension = "." + util.toScan(scan, "Extension");
 		path = home + "\\" + file + extension;
 		System.out.println("Content:");
 		try(BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
 			do {
-			    new_line = reader.readLine();
+			    new_line = scan.nextLine();
 			    if (new_line.equals("/")) break;
 			    writer.write(new_line);
 			    writer.newLine();
 			} while (true);
 		}
 		System.out.println("Created: " + path);
-	}
-
-	public LineReader getReader() {
-		return reader;
 	}
 }
--- src/main/java/com/projectfoxtrot/utils/Utils.java
+++ src/main/java/com/projectfoxtrot/utils/Utils.java
@@ -1,17 +1,11 @@
 package com.projectfoxtrot.utils;
 
-import org.jline.reader.LineReader;
+import java.util.Scanner;
 
 public class Utils {
 	
-	public String toScan(LineReader reader, String prompt) {
+	public String toScan(Scanner scan, String prompt) {
 		System.out.print(prompt + ": ");
-		return reader.readLine();
-	}
-	
-	public boolean toScanBoolean(LineReader reader, String prompt) {
-	    System.out.print(prompt + ": ");
-	    String input = reader.readLine().trim().toLowerCase();
-	    return input.equals("true") || input.equals("yes") || input.equals("y");
+		return scan.nextLine();
 	}
 }
